1ef87dcc8e25818a60e3dffe11487fca8924a329
Fildrance <fildrance@gmail.com>
2026-07-13 19:28:45 +0300

    refactor: include ICommand implementations, remove more cases of System.* inlusion, added recursiveness to loading metadata from fields/properties

--- Robust.MetadataGenerator/Helpers/CodeAnalysisHelpers.cs
+++ Robust.MetadataGenerator/Helpers/CodeAnalysisHelpers.cs
@@ -34,6 +34,7 @@ public static class CodeAnalysisHelpers
 
     /// <summary>
     /// Checks if type is derived from baseType using <seealso cref="SymbolDisplayFormat.FullyQualifiedFormat"/>.
+    /// Supports both class inheritance and interface implementation.
     /// </summary>
     public static bool IsDerivedFrom(this INamedTypeSymbol? type, INamedTypeSymbol baseType)
     {
@@ -56,19 +57,52 @@ public static class CodeAnalysisHelpers
     }
 
     /// <summary>
+    /// Checks if type matches baseType through class inheritance or interface implementation.
+    /// </summary>
+    public static bool MatchesBaseType(this INamedTypeSymbol? type, INamedTypeSymbol baseType)
+    {
+        if (type == null)
+            return false;
+
+        var fullName = baseType.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+
+        // Check class inheritance
+        var current = type;
+        while (current != null)
+        {
+            var currentName = current.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+            if (string.Equals(fullName, currentName))
+                return true;
+
+            current = current.BaseType;
+        }
+
+        // Check interface implementation
+        foreach (var iface in type.AllInterfaces)
+        {
+            var ifaceName = iface.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+            if (string.Equals(fullName, ifaceName))
+                return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
     /// Checks if a symbol has an attribute with the given name (matching by short name or metadata name).
     /// </summary>
     public static bool HasAttribute(this ISymbol symbol, string attributeName)
     {
         return symbol.GetAttributes()
             .Any(a =>
-        {
-            var className = a.AttributeClass?.Name;
-            var metadataName = a.AttributeClass?.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+                {
+                    var className = a.AttributeClass?.Name;
+                    var metadataName = a.AttributeClass?.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
 
-            return className == attributeName
-                   || metadataName == attributeName
-                   || metadataName == $"global::{attributeName}";
-        });
+                    return className == attributeName
+                           || metadataName == attributeName
+                           || metadataName == $"global::{attributeName}";
+                }
+            );
     }
 }
